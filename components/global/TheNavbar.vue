f0739c1788ea38b8ae25d31577c2853ac0f3a5e6
Ivan Gonzalez <scratchmex@gmail.com>
2021-10-06 10:47:52 -0500

    feat: add navbar link component

--- components/global/TheNavbar.vue
+++ components/global/TheNavbar.vue
@@ -1,17 +1,25 @@
 <template>
   <nav class="bg-red-100 flex justify-between">
-    <div class="text-bold">
-      <nuxt-link to="/" class="group">
-        <span class="text-green-500 group-hover:text-green-400">~{</span
-        ><span class="text-gray-700 group-hover:text-gray-500"
-          >ivan gonzalez</span
-        ><span class="text-green-500 group-hover:text-green-400">}</span>
-      </nuxt-link>
-    </div>
+    <nuxt-link to="/" v-slot="{ isExactActive, href, navigate }">
+      <TheNavbarLink :active="isExactActive" :href="href" @click="navigate">
+        <span class="group font-medium">
+          <span class="text-green-500 group-hover:text-green-400">~{</span
+          ><span class="text-gray-700 group-hover:text-gray-500"
+            >ivan gonzalez</span
+          ><span class="text-green-500 group-hover:text-green-400">}</span>
+        </span>
+      </TheNavbarLink>
+    </nuxt-link>
     <div class="flex">
-      <nuxt-link to="/blog" class="ml-8 text-gray-700 hover:text-gray-500"
-        >blog</nuxt-link
-      >
+      <nuxt-link to="/blog" v-slot="{ isExactActive, href, navigate }">
+        <TheNavbarLink
+          :active="isExactActive"
+          :href="href"
+          @click="navigate"
+          class="ml-8 text-gray-700 hover:text-gray-500"
+          >blog</TheNavbarLink
+        >
+      </nuxt-link>
     </div>
   </nav>
 </template>
---
+++ components/global/TheNavbarLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <a v-on="$listeners" :class="{ underline: active }">
+    <slot></slot>
+    <!-- <div v-if="active">-------------</div> -->
+  </a>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    active: Boolean,
+  },
+})
+</script>
