68b90daee778a2e2316ff3db67e53b6ad479c9c5
Ivan Gonzalez <scratchmex@gmail.com>
2021-10-05 21:01:47 -0500

    feat: add navbar

---
+++ components/global/TheNavbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-red-100 flex justify-between">
+        <div class="text-bold">
+            <span class="text-green-400">*</span>
+            <span class="text-gray-700">ivan gonzalez</span>
+        </div>
+        <div class="flex">
+            <nuxt-link to class="text-gray-700 hover:text-gray-500">inicio</nuxt-link>
+            <nuxt-link to class="ml-5 text-gray-700 hover:text-gray-500">blog</nuxt-link>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial class="bg-red-500"/>
+  <div class="bg-purple-200 h-screen v-screen border-4 border-black rounded-xl overflow-hidden">
+    <TheNavbar class="px-16 py-6" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import TheNavbar from '~/components/global/TheNavbar.vue'
 
 export default Vue.extend({})
+
+// TODO: firacode font
 </script>
