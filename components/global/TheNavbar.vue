d99b03f400ceb5ed6dbfc5759a23f79bbc67710e
Ivan Gonzalez <scratchmex@gmail.com>
2021-10-06 09:32:40 -0500

    refactor: move /home link in navbar

--- components/global/TheNavbar.vue
+++ components/global/TheNavbar.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="bg-red-100 flex justify-between">
     <div class="text-bold">
-      <span class="text-green-500">~{</span
-      ><span class="text-gray-700">ivan gonzalez</span
-      ><span class="text-green-500">}</span>
+      <nuxt-link to="/" class="group">
+        <span class="text-green-500 group-hover:text-green-400">~{</span
+        ><span class="text-gray-700 group-hover:text-gray-500"
+          >ivan gonzalez</span
+        ><span class="text-green-500 group-hover:text-green-400">}</span>
+      </nuxt-link>
     </div>
     <div class="flex">
-      <nuxt-link to="/" class="text-gray-700 hover:text-gray-500"
-        >inicio</nuxt-link
-      >
       <nuxt-link to="/blog" class="ml-8 text-gray-700 hover:text-gray-500"
         >blog</nuxt-link
       >
