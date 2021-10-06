af96e2da983174e19d86f54f776fc3df062332c2
Ivan Gonzalez <scratchmex@gmail.com>
2021-10-06 12:08:09 -0500

    chore: simplify navbar links / add active status

--- components/global/TheNavbar.vue
+++ components/global/TheNavbar.vue
@@ -1,23 +1,26 @@
 <template>
   <nav class="bg-red-100 flex justify-between">
     <nuxt-link to="/" v-slot="{ isExactActive, href, navigate }">
-      <TheNavbarLink :active="isExactActive" :href="href" @click="navigate">
+      <a :href="href" @click="navigate">
         <span class="group font-medium">
-          <span class="text-green-500 group-hover:text-green-400">~{</span
-          ><span class="text-gray-700 group-hover:text-gray-500"
+          <span class="text-green-500 group-hover:text-green-400"
+            >~{{ isExactActive ? ':' : '' }}{</span
+          ><span
+            class="text-gray-700 group-hover:text-gray-500"
+            :class="{ underlines: isExactActive }"
             >ivan gonzalez</span
           ><span class="text-green-500 group-hover:text-green-400">}</span>
         </span>
-      </TheNavbarLink>
+      </a>
     </nuxt-link>
     <div class="flex">
       <nuxt-link to="/blog" v-slot="{ isExactActive, href, navigate }">
-        <TheNavbarLink
-          :active="isExactActive"
+        <a
           :href="href"
           @click="navigate"
           class="ml-8 text-gray-700 hover:text-gray-500"
-          >blog</TheNavbarLink
+          :class="{ underline: isExactActive }"
+          >blog</a
         >
       </nuxt-link>
     </div>
