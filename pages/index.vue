0184d689d7817996b169ae823004ab773fe4a170
Ivan Gonzalez <scratchmex@gmail.com>
2022-08-06 18:22:21 -0500

    feat: infinite text sliding animation (marquee) (finally)

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,17 @@
 <script setup lang="ts">
-import { Ref } from 'vue'
-
 const { data } = await useAsyncData('home', () =>
   queryContent().where({ _path: '/' }).findOne()
 )
-
-async function initSlide(el: Ref<HTMLElement>) {
-  let right = true
-  let timeoutid
-
-  const toggle = () => {
-    el.value.style.transform = right
-      ? 'translateX(calc(-100% + 100vw))'
-      : 'translateX(0)'
-
-    right = !right
-
-    timeoutid = setTimeout(() => toggle(), 15000 + 100)
-  }
-
-  onMounted(() => {
-    el.value.classList.add('duration-[15000ms]')
-
-    setTimeout(() => toggle(), 10)
-  })
-
-  onUnmounted(() => {
-    clearTimeout(timeoutid)
-  })
-}
-
-const looping_text_container = ref<HTMLElement>()
-initSlide(looping_text_container)
 </script>
 
 <template>
   <main>
     <!-- sliding title -->
     <div
-      class="h-[calc(100vh/12)] lg:h-[calc(2*100vh/12)] bg-blue-300 py-2 flex items-center border-b-2 border-gray-700"
+      class="h-[calc(100vh/12)] lg:h-[calc(2*100vh/12)] bg-blue-300 py-2 flex items-center border-b-2 border-gray-700 overflow-x-hidden"
     >
-      <div
-        ref="looping_text_container"
-        class="shrink-0 flex transition-transform ease-linear"
-      >
-        <div v-for="i in 2" class="text-4xl lg:text-8xl">
+      <div class="inline-flex marquee">
+        <div v-for="i in 4" class="text-4xl lg:text-8xl whitespace-nowrap">
           <span class="mr-8">•</span>
           <span class="mr-8">Aló, me llamo Ivan González</span>
         </div>
@@ -71,3 +38,24 @@ initSlide(looping_text_container)
     </section>
   </main>
 </template>
+
+<style>
+.marquee {
+  animation: marquee 10s linear infinite;
+  will-change: transform;
+
+  @screen lg {
+    animation: marquee 15s linear infinite;
+  }
+}
+
+@keyframes marquee {
+  /* from {
+    transform: translateX(0%);
+  } */
+
+  to {
+    transform: translateX(-50%);
+  }
+}
+</style>
